b0cb6f63e6ca0b1dfefc093b4c10d519310d9f22
auriellove <auriel.e.love@gmail.com>
2022-05-21 19:29:23 -0400

    added divs for the images and started the Dom function for the timer in js

---
+++ script.js
@@ -0,0 +1,8 @@
+//the timer, per research is a dom function. it will be activated whent the start button is pushed.
+//im still playing with research to get the function right, but it should be in js not html. 
+//I got it started for reference. :-)
+
+const timer= document.getElementById("StartButton")
+timer.addEventListener("click", () => {
+
+})
