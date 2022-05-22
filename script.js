2e282df729477212b1e1cfd45da530169cb31202
KatSchultz <ks.katieschultz@gmail.com>
2022-05-21 21:20:43 -0400

    added timer display. start button begins countdown

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 //the timer, per research is a dom function. it will be activated whent the start button is pushed.
-//im still playing with research to get the function right, but it should be in js not html. 
+//im still playing with research to get the function right, but it should be in js not html.
 //I got it started for reference. :-)
 
-const timer= document.getElementById("StartButton")
-timer.addEventListener("click", () => {
+const startTime = document.getElementById("StartButton");
+const resetTime = document.getElementById("ResetButton");
+const timeDisplay = document.getElementById("timeDisplay");
+let second = timeDisplay.innerText;
+let startIntervalId;
 
-})
+function updateTimer() {
+  timeDisplay.innerText = 20;
+  second = Number(second) - 1;
+  timeDisplay.innerText = second;
+}
+
+function countDown() {
+  setInterval(updateTimer, 1000);
+}
+
+startTime.addEventListener("click", countDown);
+resetTime.addEventListener("click", countDown);
